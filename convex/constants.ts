880c7a4db786ac1b72e49f13fe4eb05bb7e8f1dc
william vansickle <vansicklewilly@gmail.com>
2026-03-21 18:28:13 -0400

    Set max conversation turns to 10, increase typing timeout to 120s

--- convex/constants.ts
+++ convex/constants.ts
@@ -39,7 +39,7 @@ export const AWKWARD_CONVERSATION_TIMEOUT = 20_000;
 export const MAX_CONVERSATION_DURATION = 15 * 60_000;
 
 // Leave a conversation if it has more than 25 messages;
-export const MAX_CONVERSATION_MESSAGES = 25;
+export const MAX_CONVERSATION_MESSAGES = 10;
 
 // Wait for 1s after sending an input to the engine. We can remove this
 // once we can await on an input being processed.
